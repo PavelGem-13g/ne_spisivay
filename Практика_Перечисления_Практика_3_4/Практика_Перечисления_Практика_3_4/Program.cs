759d658b5afffa1d2325880cac1786e91c8aef50
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-01-21 13:56:26 +0300

    Перечисления

---
+++ Практика_Перечисления_Практика_3_4/Практика_Перечисления_Практика_3_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_Перечисления_Практика_3_4
+{
+    class Program
+    {
+        enum days {Понедельник=1,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье };
+        public static int sdvig(int day, int sdvig) 
+        {
+            if (sdvig / 7 >= 0) sdvig = sdvig - (sdvig / 7) * 7;
+            day = day + sdvig;           
+            return day;
+        }
+        static void Main(string[] args)
+        {
+            for (days i=days.Понедельник; i <= days.Воскресенье; i++) Console.WriteLine("День недели:\"{0}\" соответствует числу {1}",i,(int)i);
+            Console.WriteLine("Введите день недели ");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во дней ");
+            int sdvignumber = int.Parse(Console.ReadLine());
+            day = sdvig(day, sdvignumber);
+            days result = (days)day;
+            Console.WriteLine("Черз введённое кол-во дней наступит : {0}, {1} день недели",result,(int)result);
+            Console.ReadKey();
+        }
+    }
+}
