e94eefab9f62a35df20e2d7d76723168f8cbcc23
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-01-28 22:36:52 +0300

    Практика_Структуры

---
+++ Практика_Структуры/Практика_Структуры/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+namespace Dcoder
+{
+
+    public class Program
+    {
+        public struct TMovie
+        {
+            public string Author;
+            public string Name;
+            public int Year;
+            public int PlayTime;
+        }
+        public static TMovie inputStruct ()
+        {
+            TMovie input;
+            input.Author = Console.ReadLine();
+            input.Name = Console.ReadLine();
+            input.Year = int.Parse(Console.ReadLine());
+            input.PlayTime = int.Parse(Console.ReadLine());
+            return input;
+        }
+        public static void outputStruct(TMovie output)
+        {
+            Console.WriteLine("Режисер:{0}\nНазвание:{1}\nГод выпуска:{2}\n", output.Author, output.Name, output.Year);
+        }
+        public static void outputTime(TMovie time)
+        {
+            int minutes = time.PlayTime % 60;
+            int hors = (time.PlayTime - minutes)/ 60;
+            Console.Write("Время просмотра:{0}мин или {1}ч и {2}мин",time.PlayTime,hors,minutes);
+        }
+        public static void Main(string[] args)
+        {
+            TMovie film;
+            film = inputStruct();
+            outputStruct(film);
+            outputTime(film);
+            Console.ReadKey();
+        }
+    }
+}
