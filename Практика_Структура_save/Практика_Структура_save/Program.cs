1ec180237e695981393c1d8810f081e58bb9afb2
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-01-31 13:16:17 +0300

    save mas

--- Практика_Структура_save/Практика_Структура_save/Program.cs
+++ Практика_Структура_save/Практика_Структура_save/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Практика_Структура_save
@@ -19,37 +15,46 @@ namespace Практика_Структура_save
         public static TMovie inputStruct()
         {
             TMovie input;
-            Console.Write("Введите имя автора ");
+            Console.Write("\nВведите имя автора: ");
             input.Author = Console.ReadLine();
-            Console.Write("Введите название фильма ");
+            Console.Write("Введите название фильма: ");
             input.Name = Console.ReadLine();
-            Console.Write("Введите год выпуска фильма ");
+            Console.Write("Введите год выпуска фильма: ");
             input.Year = int.Parse(Console.ReadLine());
-            Console.Write("Введите продолжительность фильма ");
+            Console.Write("Введите продолжительность фильма: ");
             input.PlayTime = int.Parse(Console.ReadLine());
             return input;
         }
         public static string outputStruct(TMovie output)
         {
-            return$"Режисер:{output.Author}\nНазвание:{output.Name}\nГод выпуска:{output.Year}\n";
+            return $"Режисер:{output.Author}\nНазвание:{output.Name}\nГод выпуска:{output.Year}\n";
         }
         public static string outputTime(TMovie time)
         {
             int minutes = time.PlayTime % 60;
             int hors = (time.PlayTime - minutes) / 60;
-            string temp = $"Время просмотра:{time.PlayTime}мин или {hors}ч и {minutes}мин\n";
+            string temp = $"Время просмотра: {time.PlayTime}мин или {hors}ч и {minutes}мин\n";
             if (time.PlayTime > 120) temp += "Фильм слишком длинный";
             if (time.PlayTime < 50) temp += "Фильм слишком короткий";
             return temp;
         }
         public static void Main(string[] args)
         {
-            TMovie film;
-            string endResult="";
-            StreamWriter save = new StreamWriter("C:\\Users\\pashe\\source\\repos\\Практика_Структура_save\\Практика_Структура_save\\Save\\save.txt");
-            film = inputStruct();
-            endResult += outputStruct(film);
-            endResult += outputTime(film);
+
+            TMovie temp;
+            string endResult = "";
+            StreamWriter save = new StreamWriter("C:\\Users\\Павел\\Documents\\GitHub\\ne_spisivay\\Практика_Структуры\\Практика_Структуры\\Save\\save.txt");
+            Console.Write("Введите кол-во фильмов: ");
+            int a = int.Parse(Console.ReadLine());
+            TMovie[] film = new TMovie[a];
+            for (int i = 0; i < a; i++)
+            {
+                temp = inputStruct();
+                endResult += outputStruct(temp);
+                endResult += outputTime(temp);
+
+                film[i] = temp;
+            }          
             Console.Write(endResult);
             save.WriteLine(endResult);
             save.Close();
--- Практика_Структуры/Практика_Структуры/Program.cs
+++ Практика_Структуры/Практика_Структуры/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-
-
-
 namespace Практика_Структуры
 {
 
