050f87d0b9909326f11c92447bd28c7f546abffe
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-02-06 22:44:11 +0300

    КОнченные классы
    коНЕчные классы

--- Практика_Классы/Практика_Классы/Program.cs
+++ Практика_Классы/Практика_Классы/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Практика_Классы
 {
@@ -11,19 +7,19 @@ namespace Практика_Классы
         public int x;
         public int y;
         public int z;
-        public void startZerPos()
+        public void startPos()
         {
             x = 0;
             y = 0;
             z = 0;
         }
-        public void startUserPos(int userX, int userY, int userZ)
+        public void startPos(int userX, int userY, int userZ)
         {
             x = userX;
             y = userY;
             z = userZ;
         }
-        public void movig(int moveX,int moveY, int moveZ)
+        public void movig(int moveX, int moveY, int moveZ)
         {
             x += moveX;
             y += moveY;
@@ -31,7 +27,7 @@ namespace Практика_Классы
         }
         public void output()
         {
-            Console.WriteLine("x={0} y={1} z={2}",x,y,z);
+            Console.WriteLine("x={0} y={1} z={2}", x, y, z);
         }
     }
     class Program
@@ -40,20 +36,39 @@ namespace Практика_Классы
         static void Main(string[] args)
         {
             Point3D point = new Point3D();
-            int x=0, y=0, z=0;
-            point.startZerPos();
+            int x = 0, y = 0, z = 0, createPoint = 0, direct = 0, delta = 0;
+            do
+            {
+                Console.Write("Введите способ создания точки: 1 - точка находится в пересечении координат, 2 - Вы сами вводите координату ");
+                createPoint = int.Parse(Console.ReadLine());
+            } while (!(0 < createPoint && createPoint < 3));
+            if (createPoint == 1) point.startPos();
+            else
+            {
+                Console.Write("Введите начальное x ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Введите начальное y ");
+                y = int.Parse(Console.ReadLine());
+                Console.Write("Введите начальное z ");
+                z = int.Parse(Console.ReadLine());
+                point.startPos(x, y, z);
+            }
             point.output();
-            Console.WriteLine("Введите начальное x");
-            x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите начальное y");
-            y = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите начальное z");
-            z = int.Parse(Console.ReadLine());
-            point.startUserPos(x, y, z);
+            while (true)
+            {
+            Console.Write("Введите ось, по которой будет совершаться перемещение: 1 - x, 2 - y, 3 - z ");
+            direct = int.Parse(Console.ReadLine());
+            if ( 0< direct && direct < 4)
+            {
+                Console.Write("Введите насколько передвинется точка ");
+                delta = int.Parse(Console.ReadLine());
+            }
+            if (direct == 1) point.movig(delta, 0, 0);
+            if (direct == 2) point.movig(0, delta, 0);
+            if (direct == 3) point.movig(0, 0, delta);
             point.output();
+            }
 
-            point.movig(5,0,0);
-            point.output();
             Console.ReadKey();
         }
     }
--- Практика_Перечисления_Практика_3_4/Практика_Перечисления_Практика_3_4/Program.cs
+++ Практика_Перечисления_Практика_3_4/Практика_Перечисления_Практика_3_4/Program.cs
@@ -17,14 +17,14 @@ namespace Практика_Перечисления_Практика_3_4
         }
         static void Main(string[] args)
         {
-            for (days i=days.Понедельник; i <= days.Воскресенье; i++) Console.WriteLine("День недели:\"{0}\" соответствует числу {1}",i,(int)i);
+            for (days i = days.Понедельник; i <= days.Воскресенье; i++) Console.WriteLine("День недели:\"{0}\" соответствует числу {1}", i, (int)i);
             Console.WriteLine("Введите день недели ");
             int day = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите кол-во дней ");
             int sdvignumber = int.Parse(Console.ReadLine());
             day = sdvig(day, sdvignumber);
             days result = (days)day;
-            Console.WriteLine("Черз введённое кол-во дней наступит : {0}, {1} день недели",result,(int)result);
+            Console.WriteLine("Черз введённое кол-во дней наступит : {0}, {1} день недели", result, (int)result);
             Console.ReadKey();
         }
     }
