d2049941fbf3c8c7a89baa414d892f0037cbc1a9
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-02-11 10:42:27 +0300

    рабочий класс

--- Практика_Классы/Практика_Классы/Program.cs
+++ Практика_Классы/Практика_Классы/Program.cs
@@ -7,13 +7,13 @@ namespace Практика_Классы
         private int x;
         private int y;
         private int z;
-        public void startPos()
+        public Point3D()
         {
             x = 0;
             y = 0;
             z = 0;
         }
-        public void startPos(int x, int y, int z)
+        public Point3D(int x, int y, int z)
         {
             this.x = x;
             this.y = y;
@@ -38,20 +38,28 @@ namespace Практика_Классы
                 default: Console.WriteLine("x={0} y={1} z={2}", x, y, z); ; break;
             }
         }
+        public double rad() 
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+        public Point3D sum(Point3D obj) 
+        {
+            return new Point3D(obj.x + this.x, obj.y + this.y, obj.z + this.z);
+        }
     }
     class Program
     {
 
         static void Main(string[] args)
         {
-            Point3D point = new Point3D();
+            Point3D point;
             int x, y, z, createPoint;
             do
             {
-                Console.Write("Введите способ создания точки: 1 - точка находится в пересечении координат, 2 - Вы сами вводите координату ");
+                Console.Write("Введите способ создания точки: 1 - точка находится в пересечении координат, 2 - Вы сами вводите координату (первая точка) ");
                 createPoint = int.Parse(Console.ReadLine());
             } while (!(0 < createPoint && createPoint < 3));
-            if (createPoint == 1) point.startPos();
+            if (createPoint == 1) point = new Point3D();
             else
             {
                 Console.Write("Введите начальное x ");
@@ -60,25 +68,64 @@ namespace Практика_Классы
                 y = int.Parse(Console.ReadLine());
                 Console.Write("Введите начальное z ");
                 z = int.Parse(Console.ReadLine());
-                point.startPos(x, y, z);
-            }
-            point.output(0);
+                //point.startPos(x, y, z);
+                point = new Point3D(x,y,z);
+            }Console.Write("Первая точка "); point.output(0);
+
+            Point3D point2;
+            do
+            {
+                Console.Write("Введите способ создания точки: 1 - точка находится в пересечении координат, 2 - Вы сами вводите координату (вторая точка) ");
+                createPoint = int.Parse(Console.ReadLine());
+            } while (!(0 < createPoint && createPoint < 3));
+            if (createPoint == 1) point2 = new Point3D();
+            else
+            {
+                Console.Write("Введите начальное x ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Введите начальное y ");
+                y = int.Parse(Console.ReadLine());
+                Console.Write("Введите начальное z ");
+                z = int.Parse(Console.ReadLine());
+                //point.startPos(x, y, z);
+                point2 = new Point3D(x, y, z);
+            }Console.Write("Вторая точка "); point2.output(0);
+
             int outdirect, direct, delta=0;
             while (true)
             {
-                Console.Write("Введите ось, по которой будет совершаться перемещение: 1 - x, 2 - y, 3 - z. Введите 0 для выхода из программы ");
+                Console.Write("Введите ось, по которой будет совершаться перемещение: 1 - x, 2 - y, 3 - z. Введите 0 для выхода из программы (первая точка)");
                 direct = int.Parse(Console.ReadLine());
                 if (direct == 0) break;
                 if (0 < direct && direct < 4)
                 {
                     Console.Write("Введите насколько передвинется точка ");
                     delta = int.Parse(Console.ReadLine());
-                }
-                
-                point.movig(direct, delta);
-                Console.Write("Введите ось для вывода: 1 - x, 2 - y, 3 - z. Введите 0 для вывода всех переменных ");
+                }point.movig(direct, delta);
+
+                Console.Write("Введите ось, по которой будет совершаться перемещение: 1 - x, 2 - y, 3 - z. Введите 0 для выхода из программы (вторая точка)");
+                direct = int.Parse(Console.ReadLine());
+                if (direct == 0) break;
+                if (0 < direct && direct < 4)
+                {
+                    Console.Write("Введите насколько передвинется точка ");
+                    delta = int.Parse(Console.ReadLine());
+                }point2.movig(direct, delta);
+
+                Console.Write("Введите ось для вывода: 1 - x, 2 - y, 3 - z. Введите 0 для вывода всех переменных (первая точка)");
                 outdirect = int.Parse(Console.ReadLine());
                 point.output(outdirect);
+
+                Console.Write("Введите ось для вывода: 1 - x, 2 - y, 3 - z. Введите 0 для вывода всех переменных (вторая точка)");
+                outdirect = int.Parse(Console.ReadLine());
+                point2.output(outdirect);
+
+                Console.WriteLine($"Радиус-вектор от начала координат {point.rad()} (первая точка)") ;
+
+                Console.WriteLine("Суммирование точек (вывод координат новой точки)");
+                Point3D sumPoint = point.sum(point2);
+                sumPoint.output(0);
+
             }
         }
     }
