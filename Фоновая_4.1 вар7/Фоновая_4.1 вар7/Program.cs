d2fa5aedbe30ab28ffffa237bbade84ac1f069a0
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-02-27 20:18:05 +0300

    длж

--- Фоновая_4.1 вар7/Фоновая_4.1 вар7/Program.cs
+++ Фоновая_4.1 вар7/Фоновая_4.1 вар7/Program.cs
@@ -15,69 +15,112 @@ namespace Фоновая_4._1_вар7
             this.a = 5;
             this.b = 2;
         }
-        private double excantiatet()
+        public double A
         {
-            return Math.Sqrt(1 - ((b  * b ) / (a  * a )));
+            set
+            {
+                try { a = value; }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено не число");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Введено слишком большое число");
+                }
+            }
+            get { return a - 2; }
+        }
+        public double B
+        {
+            set
+            {
+                try
+                {
+                    if (value > 100) throw new Exception("Введено слишком большое значение");
+                    b = value;
+                }
+                catch (Exception error) { Console.WriteLine("Ошибка: {0}", error.Message); }
+            }
+            get { return b - 2; }
+        }
+        private double excantiatet
+        {
+            get { return Math.Sqrt(1 - ((b * b) / (a * a))); }
         }
         public double focalParam()
         {
-            return (b  * b ) / a ;
+            return (b * b) / a;
         }
-        public double S()
+        public double S
         {
-            return Math.PI * a * b;
+            get { return Math.PI * a * b; }
         }
-        public double P()
+        public double P
         {
-            return  (4 *(Math.PI * a * b + (a - b) * (a - b) ))/ (a + b);
+            get { return (4 * (Math.PI * a * b + (a - b) * (a - b))) / (a + b); }
         }
-        public double A()
+        public double szhatie
         {
-            return a*2;
+            get { return b / a; }
         }
-        public double B()
+        public double focalL
         {
-            return b*2;
+            get { return a * excantiatet; }
+        }
+        public double preF
+        {
+            get { return a * (1 - excantiatet); }
+        }
+        public double apoF
+        {
+            get { return a * (1 + excantiatet); }
         }
         public double radius(double z)
         {
-            return (b) / Math.Sqrt(1.00- excantiatet() * excantiatet() * Math.Cos(z) * Math.Cos(z));
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double a, b;
-            Eleps oval;
-            Console.Write("Использовать стандартный конструктор, 1-да, другая цифра-нет ");
-            int construction = int.Parse(Console.ReadLine());
-            if (construction != 1)
+            try
             {
-                Console.Write("Введите длину (большей) оси a ");
-                a = double.Parse(Console.ReadLine());
-                Console.Write("Введите длину (меньшей) оси b ");
-                b = double.Parse(Console.ReadLine());
-                oval = new Eleps(a, b); 
+                if ((b) / Math.Sqrt(1.00 - excantiatet * excantiatet * Math.Cos(z) * Math.Cos(z)) > 0) return (b) / Math.Sqrt(1.00 - excantiatet * excantiatet * Math.Cos(z) * Math.Cos(z));
+                else throw new Exception("Радиус не существует, значение равно 0");
             }
-            else oval = new Eleps();
+            catch(Exception error) { Console.WriteLine("Ошибка: {0}", error.Message);return 0; }
+        }
 
-            Console.WriteLine("Фокальный параметр эллипса {0:F3}", oval.focalParam());
-            Console.WriteLine("Площадь  {0:F3}", oval.S());
-            Console.WriteLine("Периметр  {0:F3}", oval.P());
-            Console.Write("Введите угол (для радиуса) ");
-            double ugol = double.Parse(Console.ReadLine());
-            // 57.2958)
-            Console.WriteLine("Радиус  {0:F3}", oval.radius(ugol*Math.PI/180));
-            int choseAB;
-            do
+        }
+        class Program
+        {
+            static void Main(string[] args)
             {
-                Console.WriteLine("Получить a-1 или b-2");
-                choseAB = int.Parse(Console.ReadLine());
-            } while (!(0< choseAB && choseAB<3));
-            if (choseAB == 1) Console.WriteLine("а = {0}",oval.A());
-            if (choseAB == 2) Console.WriteLine("b = {0}", oval.B());
-            Console.ReadKey();
+                double a, b;
+                Eleps oval;
+                Console.Write("Использовать стандартный конструктор, 1-да, другая цифра-нет ");
+                int construction = int.Parse(Console.ReadLine());
+                if (construction != 1)
+                {
+                    Console.Write("Введите длину (большей) оси a ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.Write("Введите длину (меньшей) оси b ");
+                    b = double.Parse(Console.ReadLine());
+                    oval = new Eleps(a, b);
+                }
+                else oval = new Eleps();
+
+                Console.WriteLine("Фокальный параметр эллипса {0:F3}", oval.focalParam());
+                Console.WriteLine("Площадь  {0:F3}", oval.S());
+                Console.WriteLine("Периметр  {0:F3}", oval.P());
+                Console.Write("Введите угол (для радиуса) ");
+                double ugol = double.Parse(Console.ReadLine());
+                // 57.2958)
+                Console.WriteLine("Радиус  {0:F3}", oval.radius(ugol * Math.PI / 180));
+                int choseAB;
+                do
+                {
+                    Console.WriteLine("Получить a-1 или b-2");
+                    choseAB = int.Parse(Console.ReadLine());
+                } while (!(0 < choseAB && choseAB < 3));
+                if (choseAB == 1) Console.WriteLine("а = {0}", oval.A());
+                if (choseAB == 2) Console.WriteLine("b = {0}", oval.B());
+                Console.ReadKey();
+            }
         }
     }
-}
