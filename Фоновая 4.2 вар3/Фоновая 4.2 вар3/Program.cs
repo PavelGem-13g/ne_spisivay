76a27bb350ebf6dfea26609f121cd52c4f35bb04
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-03-10 21:37:43 +0300

    Фоновая 4.2

---
+++ Фоновая 4.2 вар3/Фоновая 4.2 вар3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фоновая_4._2_вар3
+{
+    class MatrixWeather
+    {
+        int month;
+        int day;
+        int[,] temperature;
+        public MatrixWeather() 
+        {
+            day = 1;
+            month = 1;
+            temperature = new int[6,7];
+        }
+
+        public MatrixWeather(int day, int month, int week)
+        {
+            this.day = day;
+            this.month = month;
+            temperature = new int[week,7];
+        }
+        static void rndTemp() 
+        {
+            Random rnd = new Random();
+            for (int j = 0; j < temperature.GetLength(1); j++) 
+            { 
+                for (int i = 0; i < 8; i++) 
+                {
+                    temperature[i, j] = rnd.Next();
+                }
+            
+            }
+        }
+    }
+    enum Months { январь = 1, февраль, март, апрель, май, июнь, июль, август, сенябрь, октябрь, ноябрь, декабрь };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
