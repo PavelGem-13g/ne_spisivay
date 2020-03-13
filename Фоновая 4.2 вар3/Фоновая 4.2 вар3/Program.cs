5d73c62ae2dcf96705bb0e1fa5908b5c583dce62
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-03-13 13:25:14 +0300

    qweqwe

--- Фоновая 4.2 вар3/Фоновая 4.2 вар3/Program.cs
+++ Фоновая 4.2 вар3/Фоновая 4.2 вар3/Program.cs
@@ -15,16 +15,18 @@ namespace Фоновая_4._2_вар3
         {
             day = 1;
             month = 1;
-            temperature = new int[6,7];
+            temperature = new int[5,7];
         }
 
-        public MatrixWeather(int day, int month, int week)
+        public MatrixWeather(int day, int month)
         {
+            
             this.day = day;
             this.month = month;
-            temperature = new int[week,7];
+            int week;
+            temperature = new int[6,7];
         }
-        static void rndTemp() 
+        static MatrixWeather rndTemp() 
         {
             Random rnd = new Random();
             for (int j = 0; j < temperature.GetLength(1); j++) 
@@ -35,7 +37,40 @@ namespace Фоновая_4._2_вар3
                 }
             
             }
+            return MatrixWeather;
         }
+        public output()
+        {
+            int k=0;
+            for (int j = 0; j < temperature.GetLength(1); j++) 
+            { 
+                for (int i = 0; i < 8; i++) 
+                {
+                    if(temperature[i,j]>Int32.MinValue)
+                    {
+                    k++;
+                        Console.WriteLine(k);
+                    }
+                    Console.WriteLine(temperature[i,j]);
+                }
+            
+            }
+            
+        }
+      public int scach() 
+        {
+            inr max=0,maxTemp=0;;
+            for (int j = 0; j < temperature.GetLength(1)-2; j++) 
+            { 
+                for (int i = 0; i < 7; i++) 
+                {
+                    maxTemp=Math.Abs(Math.Abs(temperature[i, j])-Math.Abs(temperature[i+1, j+1]));
+                }
+            if(maxTemp>max)max=maxTemp;
+            }
+          return max;
+        }
+        
     }
     enum Months { январь = 1, февраль, март, апрель, май, июнь, июль, август, сенябрь, октябрь, ноябрь, декабрь };
 
