0840c94f1613b958817a5aafa1ba764a2d6b2593
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-05-15 22:29:49 +0300

    фоновая 6.1 со звуком

---
+++ Простые формы/Фоновая 6.1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Фоновая_6._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(125, 60);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(75, 23);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(270, 60);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(75, 23);
+            this.B.TabIndex = 1;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 153);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Веселая форма";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+    }
+}
+
---
+++ Простые формы/Фоновая 6.1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Фоновая_6._1
+{
+
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        SoundPlayer soundPlayer = new SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\s.wav"))
+            {
+                soundPlayer.SoundLocation = Directory.GetCurrentDirectory() + "\\s.wav";
+                //..\Music\
+                soundPlayer.Load();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A_Click(object sender, EventArgs e)
+        {
+            if (B.Location.X - A.Location.X >= 65)
+            {
+                B.Location = new Point(B.Location.X - 5, B.Location.Y);
+                A.Location = new Point(A.Location.X + 5, A.Location.Y);
+                ColorCanger();
+
+            }
+
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            if (A.Location.X > 5)
+            {
+                A.Location = new Point(A.Location.X - 5, A.Location.Y);
+                ColorCanger();
+            }
+            if (B.Location.X < Size.Width - 80)
+            {
+                B.Location = new Point(B.Location.X + 5, B.Location.Y);
+                ColorCanger();
+            }
+        }
+        void ColorCanger()
+        {
+            A.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+            B.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+            BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\s.wav"))
+            {
+                soundPlayer.Play();
+                ///SystemSounds.Beep.Play();
+            }
+
+
+        }
+
+    }
+}
