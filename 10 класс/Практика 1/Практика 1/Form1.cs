bb3055d7aeb981c077da314f09fc60a7c302f59f
PavelGem <pashentsev.1@gmail.com>
2020-09-04 23:40:19 +0300

    практика 1 и наброски музыки

--- 10 класс/Практика 1/Практика 1/Form1.cs
+++ 10 класс/Практика 1/Практика 1/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Практика_1
@@ -7,14 +8,16 @@ namespace Практика_1
     public partial class Form1 : Form
     {
         Point[] points;
+        //SoundPlayer soundPlayer = new SoundPlayer();
         public Form1()
         {
             InitializeComponent();
-            points = new Point[] { 
-                new Point(containers.Panel2.Width / 4, containers.Panel2.Height / 5 * 4), 
-                new Point(containers.Panel2.Width / 4 * 3, containers.Panel2.Height / 5 * 4), 
+            points = new Point[] {
+                new Point(containers.Panel2.Width / 4, containers.Panel2.Height / 5 * 4),
+                new Point(containers.Panel2.Width / 4 * 3, containers.Panel2.Height / 5 * 4),
                 new Point(containers.Panel2.Width / 2, containers.Panel2.Height / 5) };
-
+            //soundPlayer  = new SoundPlayer();
+            //soundPlayer.Stream = Properties.Resources.music;
         }
 
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
@@ -25,14 +28,33 @@ namespace Практика_1
         private void button_Click(object sender, EventArgs e)
         {
             Increase_triangle();
-            Invalidate();
+            //soundPlayer.Play();
         }
 
         void Increase_triangle()
         {
-            points[0] = new Point(points[0].X - 5, points[0].Y + 5);
-            points[1] = new Point(points[1].X + 5, points[1].Y + 5);
-            points[2] = new Point(points[2].X - 5, points[2].Y - 5);
+            if (points[0].X > 0)
+            {
+                points[0] = new Point(points[0].X - 5, points[0].Y);
+            }
+            if (points[0].Y < containers.Panel2.Height)
+            {
+                points[0] = new Point(points[0].X, points[0].Y + 5);
+            }
+            if (points[1].X < containers.Panel2.Width)
+            {
+                points[1] = new Point(points[1].X + 5, points[1].Y);
+            }
+            if (points[1].Y < containers.Panel2.Height)
+            {
+                points[1] = new Point(points[1].X, points[1].Y + 5);
+            }
+            if (points[2].Y > 0)
+            {
+                points[2] = new Point(points[2].X, points[2].Y - 5);
+            }
+
+            containers.Panel2.Invalidate();
         }
     }
 }
