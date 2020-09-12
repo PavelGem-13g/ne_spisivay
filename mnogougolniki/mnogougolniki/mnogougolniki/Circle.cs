ee67fe916396627970535f50f220d9046153baf6
PavelGem <pashentsev.1@gmail.com>
2020-09-13 01:23:17 +0300

    mnogougolniki upload

---
+++ mnogougolniki/mnogougolniki/mnogougolniki/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mnogougolniki
+{
+    class Circle:Shape
+    {
+        public Circle(int x, int y,):base(x,y)
+        {
+            
+        }
+    }
+}
---
+++ mnogougolniki/mnogougolniki/mnogougolniki/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace mnogougolniki
+{
+    abstract class Shape
+    {
+        static int R;
+        static Color lineColor;
+        static Color fillColor;
+        int x;
+        int y;
+        static Shape()
+        {
+            R = 1;
+            lineColor = Color.Black;
+            fillColor = Color.Black;
+        }
+        public Shape(int x, int y, int R, Color lineColor, Color fillColor)
+        {
+            this.x = x;
+            this.y = y;
+            this.R = R;
+
+        }
+        public virtual void Draw(Graphics g)
+        {
+            Application.Exit();
+        }
+        public virtual bool IsInside(Point pos)
+        {
+            return true;
+        }
+    }
+}
