04569a43e56ff64419c1b79617a2a30cc309a345
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-09-20 00:07:23 +0300

    update

--- mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
+++ mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
@@ -13,67 +13,84 @@ namespace mnogougolniki
 {
     public partial class Form1 : Form
     {
-        //Collection<Shape> shapes;
-        Sqare circle;
+        Collection<Shape> shapes;
+        //Sqare circle;
         public Form1()
         {
             InitializeComponent();
-            //shapes = new Collection<Shape>();
-            circle = new Sqare(ClientSize.Width/2,ClientSize.Height/2);
+            shapes = new Collection<Shape>();
+            //circle = new Sqare(ClientSize.Width/2,ClientSize.Height/2);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (circle.IsMovable) 
+            /*if (circle.IsMovable) 
             {
                 circle.X = e.X;
                 circle.Y = e.Y;
+            }*/
+            foreach (var item in shapes)
+            {
+                if (item.IsMovable) 
+                {
+                    item.X = e.X;
+                    item.Y = e.Y;
+                }
             }
             Refresh();
+            
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
+        {/*
             if (circle.IsInside(e.Location)) 
             {
                 circle.IsMovable = true;
-            }
+            }*/
             //наработки для коллекцией 
-            /*foreach (var item in shapes)
+            foreach (var item in shapes)
             {
                 if (item.IsInside(e.Location))
                 {
-                    foreach (var changer in shapes)
-                    {
-                        changer.IsMovable = true;
-                    }
-                    break;
+                    item.IsMovable = true;
                 }
-                else 
-                {
-                    //TODO shapes.Add(new Shape(e.Location.X, e.Location.Y));
-                }
-            }*/
+            }
+            if (shapes.Count == 0) 
+            {
+                shapes.Add(new Circle(e.Location.X, e.Location.Y));
+            }
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            circle.IsMovable = false;
-            //наработка для коллекций
-            /*foreach (var changer in shapes)
+            //circle.IsMovable = false;
+            //наработка для коллекции
+            int k = 0;
+            foreach (var item in shapes)
             {
-                changer.IsMovable = false;
-            }*/
+                if (item.IsMovable) 
+                {
+                    k++;
+                }
+            }
+            if (k.Equals(0)) 
+            {
+                shapes.Add(new Circle(e.X, e.Y));
+            }
+            foreach (var item in shapes)
+            {
+                item.IsMovable = false;
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            circle.Draw(e.Graphics);
+            //circle.Draw(e.Graphics);
             //наработки для коллекций
-            /*foreach (var item in shapes)
+            foreach (var item in shapes)
             {
                 item.Draw(e.Graphics);
-            }*/
+            }
         }
     }
 }
