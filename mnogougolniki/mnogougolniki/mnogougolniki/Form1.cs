c71fca32b1de64906d205fbaea74db44902ffc94
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-09-19 21:49:29 +0300

    update drag & drop, working sqaere

--- mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
+++ mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
@@ -13,21 +13,33 @@ namespace mnogougolniki
 {
     public partial class Form1 : Form
     {
-        Collection<Shape> shapes;
+        //Collection<Shape> shapes;
+        Sqare circle;
         public Form1()
         {
             InitializeComponent();
-            shapes = new Collection<Shape>();
+            //shapes = new Collection<Shape>();
+            circle = new Sqare(ClientSize.Width/2,ClientSize.Height/2);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
+            if (circle.IsMovable) 
+            {
+                circle.X = e.X;
+                circle.Y = e.Y;
+            }
             Refresh();
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            foreach (var item in shapes)
+            if (circle.IsInside(e.Location)) 
+            {
+                circle.IsMovable = true;
+            }
+            //наработки для коллекцией 
+            /*foreach (var item in shapes)
             {
                 if (item.IsInside(e.Location))
                 {
@@ -41,23 +53,27 @@ namespace mnogougolniki
                 {
                     //TODO shapes.Add(new Shape(e.Location.X, e.Location.Y));
                 }
-            }
+            }*/
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            foreach (var changer in shapes)
+            circle.IsMovable = false;
+            //наработка для коллекций
+            /*foreach (var changer in shapes)
             {
                 changer.IsMovable = false;
-            }
+            }*/
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            foreach (var item in shapes)
+            circle.Draw(e.Graphics);
+            //наработки для коллекций
+            /*foreach (var item in shapes)
             {
                 item.Draw(e.Graphics);
-            }
+            }*/
         }
     }
 }
--- mnogougolniki/mnogougolniki/mnogougolniki/Shape.cs
+++ mnogougolniki/mnogougolniki/mnogougolniki/Shape.cs
@@ -17,12 +17,20 @@ namespace mnogougolniki
             {
                 return x;
             }
+            set 
+            {
+                x = value;
+            }
         }
         public int Y 
         {
             get 
             {
                 return y;
+            }
+            set 
+            {
+                y = value;
             }
         }
         public int R 
@@ -59,7 +67,9 @@ namespace mnogougolniki
         }
         static Shape()
         {
-            r = 1;
+            r = 10;
+            lineColor = Color.Black;
+            fillColor = Color.Black;
         }
         public Shape(int x, int y)
         {
