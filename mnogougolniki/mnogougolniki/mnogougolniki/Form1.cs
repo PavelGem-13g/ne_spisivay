e3f4543d08df8ba45f54cd1a9ace688fa113a0f1
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-09-20 09:36:17 +0300

    optimaise & create Location

--- mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
+++ mnogougolniki/mnogougolniki/mnogougolniki/Form1.cs
@@ -6,50 +6,38 @@ namespace mnogougolniki
     public partial class Form1 : Form
     {
         Collection<Shape> shapes;
-        //Sqare circle;
         public Form1()
         {
             InitializeComponent();
             shapes = new Collection<Shape>();
-            //circle = new Sqare(ClientSize.Width/2,ClientSize.Height/2);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            /*if (circle.IsMovable) 
-            {
-                circle.X = e.X;
-                circle.Y = e.Y;
-            }*/
             foreach (var item in shapes)
             {
                 if (item.IsMovable)
                 {
-                    item.X = e.X;
-                    item.Y = e.Y;
+                    item.Location = e.Location;
+                    Refresh();
                 }
             }
-            Refresh();
-
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {/*
-            if (circle.IsInside(e.Location)) 
-            {
-                circle.IsMovable = true;
-            }*/
-            //наработки для коллекцией 
+        {
             if (MouseButtons.Left == e.Button)
             {
+                int k = 0;
                 foreach (var item in shapes)
                 {
                     if (item.IsInside(e.Location))
                     {
                         item.IsMovable = true;
+                        k++;
                     }
                 }
-                if (shapes.Count == 0)
+                if (shapes.Count == 0 || k == 0)
                 {
                     shapes.Add(new Circle(e.Location.X, e.Location.Y));
                 }
@@ -65,26 +53,13 @@ namespace mnogougolniki
                     }
                 }
             }
+            Refresh();
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            //circle.IsMovable = false;
-            //наработка для коллекции
             if (MouseButtons.Left == e.Button)
             {
-                int k = 0;
-                foreach (var item in shapes)
-                {
-                    if (item.IsMovable)
-                    {
-                        k++;
-                    }
-                }
-                if (k.Equals(0))
-                {
-                    shapes.Add(new Circle(e.X, e.Y));
-                }
                 foreach (var item in shapes)
                 {
                     item.IsMovable = false;
@@ -94,8 +69,6 @@ namespace mnogougolniki
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //circle.Draw(e.Graphics);
-            //наработки для коллекций
             foreach (var item in shapes)
             {
                 item.Draw(e.Graphics);
--- mnogougolniki/mnogougolniki/mnogougolniki/Shape.cs
+++ mnogougolniki/mnogougolniki/mnogougolniki/Shape.cs
@@ -17,20 +17,12 @@ namespace mnogougolniki
             {
                 return x;
             }
-            set 
-            {
-                x = value;
-            }
         }
         public int Y 
         {
             get 
             {
                 return y;
-            }
-            set 
-            {
-                y = value;
             }
         }
         public int R 
@@ -49,6 +41,18 @@ namespace mnogougolniki
             set 
             {
                 isMovable = value;
+            }
+        }
+        public Point Location 
+        {
+            get 
+            {
+                return new Point(x, y);
+            }
+            set 
+            {
+                x = value.X;
+                y = value.Y;
             }
         }
         public Color LineColor
